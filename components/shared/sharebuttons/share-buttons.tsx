04e14c40b44016816743574aa5e84beae880c07d
Derescio <ddw.web.dev.services@gmail.com>
2024-12-27 13:01:18 -0500

    Created Product Slug page to show each item. Added Prosma TS file for connectivity. Added Product Images. Updated Product Actions.

--- components/shared/sharebuttons/share-buttons.tsx
+++ components/shared/sharebuttons/share-buttons.tsx
@@ -11,19 +11,20 @@ import {
     EmailShareButton,
     EmailIcon,
 } from 'react-share';
+import { Product } from '@/types';
 
 
 
-const ShareButtons = ({ product }: { product: any }) => {
+const ShareButtons = ({ product, className }: { product: Product, className?: string }) => {
 
     const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/product/${product.slug}`;
 
     return (
         <>
-            <div className="flex flex-col items-center">
-                <h3 className="text-md text center pt-2">Don`t wish. Hint instead.</h3>
-                <div className="flex gap-3 justify-center pb-5">
-                    <FacebookShareButton url={shareUrl} hashtag={product.name}>
+            <div className="flex flex-col items-center gap-y-4">
+                <h3 className="text-md text center pt-2">Share this item with a friend.</h3>
+                <div className="flex gap-3 justify-center pb-5 ">
+                    <FacebookShareButton url={shareUrl} hashtag={product.name} >
                         <FacebookIcon size={32} round={true} />
                     </FacebookShareButton>
                     <TwitterShareButton url={shareUrl} title={product.name} hashtags={[`${product.category}`, `GreatGift`]}>
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,12 @@
 'use server';
 import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/app/db/prisma'
 import { prismaToJSObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constatnts';
 
 //Get latest product
 export const getLatestProducts = async () => {
-    const prisma = new PrismaClient();
+    // const prisma = new PrismaClient();
     const products = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: {
@@ -13,4 +14,15 @@ export const getLatestProducts = async () => {
         }
     });
     return prismaToJSObject(products);
+}
+
+//Get product by slug
+export const getProductBySlug = async (slug: string) => {
+    // const prisma = new PrismaClient();
+    const product = await prisma.product.findFirst({
+        where: {
+            slug
+        }
+    });
+    return prismaToJSObject(product);
 }
